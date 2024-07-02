ca7014ea40501867e5adb166b87fb7ed528e419a
Noushil Sharma <96626268+Noushil-Sharma@users.noreply.github.com>
2024-07-03 01:03:26 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
         </div> */}
         <div className="about-right">
             <div className="about-para">
-                <p>I am a prefinal student at IIIT Sonepat, specializing in web development and competitive programming. As a passionate problem solver and full stack developer, I enjoy creating efficient and scalable web applications. My skills span both front-end and back-end technologies, enabling me to deliver complete, user-friendly solutions. I am constantly seeking new challenges to enhance my technical expertise and contribute to impactful projects.</p>
+                <p>I am a prefinal student at IIIT Sonepat, specializing in web development and competitive programming. As a passionate problem solver and full stack developer, I enjoy creating efficient and scalable web applications. My skills span both front-end and back-end technologies, enabling me to deliver complete, user-friendly solutions. </p>
                 <p> I am constantly seeking new challenges to enhance my technical expertise and contribute to impactful projects.</p>
             </div>
             <div className="about-skills">
